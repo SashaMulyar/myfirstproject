c338e54b720287f7119936978c5798715a11f974
SashaMulyar <kreed992@gmail.com>
2021-09-28 23:58:24 +0300

    Change_test1

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
-favorite_songs = {
-'Тополиный пух': 'Иванушки international',
-'Город золотой': 'Аквариум',
-'Звезда по имени Солнце': 'Кино',
-'Группа крови': 'Кино'
-}
+t=input('Please input for how many months you want to open a deposit:\n')
+print('you entered',t)
+b=input('Please input the initial amount:\n')
+print('you entered',b)
+n=input('Please input the year rate, %:\n')
+print('you entered',n)
+total_amount=int(b)*((1+(int(n)/100)/12)**int(t))
+print('Total amount will be',round(total_amount, 2))
