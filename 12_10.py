db80cd11acc225fd3a7b829d7010514b164c3741
SashaMulyar <91182862+SashaMulyar@users.noreply.github.com>
2021-10-13 19:58:02 +0300

    ready12_10

--- 12_10.py
+++ 12_10.py
@@ -4,15 +4,20 @@ b=[]
 d={'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8, 'nine':9, 'ten':10, 'eleven':11, 'twelve':12, 'thirteen':13, 'fourteen':14, 'fiveteen':15, 'sixteen':16, 'seventeen':17, 'eighteen':18, 'nineteen':19, 'twenty':20}
 for i in l:
     b.append(d[i])
-print(b)
 
 c=[]
 for i in b:
     if not i in c:
         c.append(i)   #без дубликатов
 
-c=sorted(c)      #сортировка
-print(c) 
+# c=sorted(c)      #сортировка
+# print(c) 
+
+for w in range(len(c)-1):                  #сортировка
+    for i in range((len(c)-1)-w):
+        if c[i]>c[i+1]:
+            c[i],c[i+1]=c[i+1],c[i]
+print(c)        
 
 s=0
 for i in c:
@@ -20,7 +25,6 @@ for i in c:
         s=s+i
 print(s)        #сумма нечетных
 
-len(c)
 for i in range(len(c)-1):
     if i%2==0:
         print(c[i]*c[i+1]) #произведения
