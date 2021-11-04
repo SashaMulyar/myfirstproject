075b059f1c53e93f419883c506470fc83e11ccb2
SashaMulyar <91182862+SashaMulyar@users.noreply.github.com>
2021-11-04 17:47:48 +0300

    list

--- task5_рекурсии.py
+++ task5_рекурсии.py
@@ -1,25 +1,34 @@
-# l=[8,9,7,[7,1,4],3]
+l=[8,9,7,[7,[5,7],4],3]
+def sum1(lst):
+    total = 0
+    for element in lst:
+        if (type(element) == type([])):
+            total = total + sum1(element)
+        else:
+            total = total + element
+    return total
+print("Сумма элементов равна:", sum1(l))
 
 
 
 
-while True:
-    try:
-        x = int(input('введите число==>>')) + 1
-        break  
-    except ValueError:
-        print('неверный ввод')
-        continue
-def fib(x):
-    if x==1:
-        return 0
-    if x==2:
-        return 1
-    return fib(x-1)+fib(x-2)
-def fiblist(x):
-    l = []
-    for i in range(1,x):
-        b = fib(i)
-        l.append(b)
-    return (', '.join(map(str,l)))
-print(fiblist(x))
+# while True:
+#     try:
+#         x = int(input('введите число==>>')) + 1
+#         break  
+#     except ValueError:
+#         print('неверный ввод')
+#         continue
+# def fib(x):
+#     if x==1:
+#         return 0
+#     if x==2:
+#         return 1
+#     return fib(x-1)+fib(x-2)
+# def fiblist(x):
+#     l = []
+#     for i in range(1,x):
+#         b = fib(i)
+#         l.append(b)
+#     return (', '.join(map(str,l)))
+# print(fiblist(x))
